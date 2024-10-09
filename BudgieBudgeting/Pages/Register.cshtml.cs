864cd24016592fdab81c412b52aff153a537b92c
T00225685 <T00225685@1XXCC96E52C1466>
2024-10-09 10:11:37 +0100

    Updated login and register, login validates

---
+++ BudgieBudgeting/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- BudgieBudgeting/Pages/login.cshtml.cs
+++ BudgieBudgeting/Pages/login.cshtml.cs
@@ -1,25 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 
 namespace BudgieBudgeting.Pages
 {
-    public class loginModel : PageModel
-    {
-   
-    
+	public class loginModel : PageModel
+	{
+		[BindProperty]
+		public required Credential Credential { get; set; }
 
-        public void OnPost()
-        {
-            Response.Redirect("/Homepage");
-        }
+		public required string ErrorMessage { get; set; }
 
+		public void OnPost()
+		{
+			if (ModelState.IsValid)
+			{
+				if (Credential.Username == "admin" && Credential.Password == "admin")
+				{
+					Response.Redirect("/Homepage");
+				}
+				else
+				{
+					ErrorMessage = "Invalid username or password";
+				}
+			}
+		}
+	}
+	public class Credential
+	{
+		[Required]
+		public required string Username { get; set; }
 
-    }
-    public class Credential
-    {
-      
-      
-    }
+		[Required]
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
+	}
 }
