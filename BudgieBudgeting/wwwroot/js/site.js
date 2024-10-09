de43393c88ebde29cb53de46111feb6508f5ce9f
T00225685 <T00225685@1XXCC96E52D9867>
2024-10-09 17:32:43 +0100

    Delete requires fixing

--- BudgieBudgeting/wwwroot/js/site.js
+++ BudgieBudgeting/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    function confirmDelete() {
+        return confirm("Are you sure you want to delete your account? This action cannot be undone.");
+    }
