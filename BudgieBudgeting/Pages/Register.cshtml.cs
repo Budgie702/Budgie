fa6071eaa77b4c38573002b1f33e101d89ec52e1
danny <daniel.finnegan@students.ittralee.ie>
2024-10-09 13:09:48 +0100

    Register now has same view as login, aswell as validation

--- BudgieBudgeting/Pages/Register.cshtml.cs
+++ BudgieBudgeting/Pages/Register.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace BudgieBudgeting.Pages
 {
     public class RegisterModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public required RegisterCredential RegisterCredential { get; set; }
+
+        public required string ErrorMessage { get; set; }
+
+        public void OnPost()
         {
+            Response.Redirect("/homepage");
         }
+    }
+    public class RegisterCredential
+    {
+        [Required]
+        public required string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
     }
 }
