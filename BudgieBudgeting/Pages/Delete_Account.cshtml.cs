9f1a48c1776ce0980ae1d76c262bc78ff61d1ca2
T00225685 <T00225685@1XXCC96E52D9867>
2024-10-09 17:36:03 +0100

    Error Fix

--- BudgieBudgeting/Pages/Delete_Account.cshtml.cs
+++ BudgieBudgeting/Pages/Delete_Account.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace BudgieBudgeting.Pages
 {
     public class Delete_AccountModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public required DeleteCredential DeleteCredential { get; set; }
+
+        public required string ErrorMessage { get; set; }
+
+        public void OnPost()
         {
+            
+            Response.Redirect("/homepage");
         }
+    }
+
+    public class DeleteCredential
+    {
+        [Required]
+        public required string Username { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
     }
 }
