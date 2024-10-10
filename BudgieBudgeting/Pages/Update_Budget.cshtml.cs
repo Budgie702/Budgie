b91df48b85d9067d2392eeba3d07658a142f8cad
TurboNatee <nathan.swanepoel@students.ittralee.ie>
2024-10-10 14:35:16 +0100

    Added Update_Budget, added links to the homepage dropdown menu to the new pages that were added, and renamed Compare_Budget_and spend to Compare_Budget_and_Spend.

---
+++ BudgieBudgeting/Pages/Update_Budget.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class Update_BudgetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
