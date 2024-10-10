26d8315aaee64bbc31e6096cbcaf70291440c36f
Nathan <nathan.swanepoel@students.ittralee.ie>
2024-10-10 12:44:38 +0100

    Added Compare budget and spend page

---
+++ BudgieBudgeting/Pages/Compare_Budget_and spend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class Compare_Budget_and_spendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
