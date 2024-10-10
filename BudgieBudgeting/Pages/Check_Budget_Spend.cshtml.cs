4973d6558c718a23770658413f04a95a1ff36d28
Nathan <nathan.swanepoel@students.ittralee.ie>
2024-10-10 12:49:07 +0100

    Created Check budget spend and create budget

---
+++ BudgieBudgeting/Pages/Check_Budget_Spend.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class Check_Budget_SpendModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ BudgieBudgeting/Pages/Create_Budget.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class Create_BudgetModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
