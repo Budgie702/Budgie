b2e7d521847aa44d19a6dd230d9d5a14ff396083
Jc2772 <116795528+Jc2772@users.noreply.github.com>
2024-10-10 13:52:59 +0100

    Created database branch, please test connection. if result from query is "1" it works

---
+++ BudgieBudgeting/Connection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+using System.Data.Common;
+namespace BudgieBudgeting
+{
+    public class Conection
+    {
+        public Conection()
+        {
+            string connectionString = "Server=tcp:budgie.database.windows.net,1433;Initial Catalog=Budgie Database;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand("select * from person", connection);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+            connection.Close();
+            Console.WriteLine(ds.Tables[0].Rows[0][0]);
+        }
+    }
+}
