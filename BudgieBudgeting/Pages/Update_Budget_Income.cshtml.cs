2aa0d612b69c44a9488772de5aba1c955ba11a2c
TurboNatee <nathan.swanepoel@students.ittralee.ie>
2024-10-10 23:03:58 +0100

    Changed the name of Create Budget to Create Budget Income

---
+++ BudgieBudgeting/Pages/Update_Budget_Income.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BudgieBudgeting.Pages
+{
+    public class Update_BudgetModel : PageModel
+    {
+       
+            [BindProperty]
+            public string BudgetAmount { get; set; }
+
+            public string ErrorMessage { get; set; }
+
+        public void OnPost()
+        {
+            if (string.IsNullOrEmpty(BudgetAmount))
+            {
+                ErrorMessage = "Budget amount cannot be empty.";
+            }
+            else
+            {
+                // Handle form submission logic here, like saving the budget to the database
+                ErrorMessage = null;
+            }
+        }
+            
+        
+    }
+}
