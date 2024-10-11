2cd38401ce550696f0b9fb10999e7f3f35150e69
Daniel Finnegan <Danny.Finnegan@students.ittralee.ie>
2024-10-11 14:02:26 +0100

    Ladies and gentelmen, The database works

--- BudgieBudgeting/Pages/Register.cshtml.cs
+++ BudgieBudgeting/Pages/Register.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
-
+using Microsoft.Data.SqlClient;
 namespace BudgieBudgeting.Pages
 {
     public class RegisterModel : PageModel
@@ -13,11 +13,16 @@ namespace BudgieBudgeting.Pages
 
         public void OnPost()
         {
-            Console.WriteLine("Test");
-            Connection connection = new Connection();
-            string output = connection.GetOutput();
-            Console.WriteLine(output);
-            Response.Redirect("/homepage");
+            string sql = "INSERT INTO dbo.Customer (Username, email, UserPassword) VALUES (@Username, @email, @UserPassword);";
+            using (SqlConnection connection = DatabaseConnection.Connection)
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@Username", RegisterCredential.Username);
+                command.Parameters.AddWithValue("@email", RegisterCredential.Email);
+                command.Parameters.AddWithValue("@UserPassword", RegisterCredential.Password);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
         }
     }
     public class RegisterCredential
