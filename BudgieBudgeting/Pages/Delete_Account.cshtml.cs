e62342a6d1c4a9af031b26cfd55fd8e65b1ee8d7
danny <daniel.finnegan@students.ittralee.ie>
2024-10-14 15:08:56 +0100

    Updated DeleteAccount to be validated, now removes from the database

--- BudgieBudgeting/Pages/Delete_Account.cshtml.cs
+++ BudgieBudgeting/Pages/Delete_Account.cshtml.cs
@@ -1,26 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using System.ComponentModel.DataAnnotations;
 
 namespace BudgieBudgeting.Pages
 {
-    public class Delete_AccountModel : PageModel
+	public class Delete_AccountModel : PageModel
+	{
+		[BindProperty]
+		public DeleteCredential DeleteCredential { get; set; }
+
+		public string ErrorMessage { get; set; }
+
+		public void OnPost()
+		{
+			string DeleteQuery = "DELETE FROM dbo.Customer WHERE email = @Email AND UserPassword = @UserPassword";
+
+			using (SqlConnection connection = DatabaseConnection.Connection)
+			{
+				connection.Open();
+
+				using (SqlCommand insertCommand = new SqlCommand(DeleteQuery, connection))
+				{
+					insertCommand.Parameters.AddWithValue("@Email", DeleteCredential.Email);
+					insertCommand.Parameters.AddWithValue("@UserPassword", DeleteCredential.Password);
+
+					insertCommand.ExecuteNonQuery();
+				}
+			}
+
+			Response.Redirect("/Homepage");
+		}
+	}
+
+		public class DeleteCredential
     {
-        [BindProperty]
-        public required DeleteCredential DeleteCredential { get; set; }
-
-        public required string ErrorMessage { get; set; }
-
-        public void OnPost()
-        {
-            Response.Redirect("/homepage");
-        }
-    }
-
-    public class DeleteCredential
-    {
-        [Required]
-        public required string Username { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
         public required string Email { get; set; }
