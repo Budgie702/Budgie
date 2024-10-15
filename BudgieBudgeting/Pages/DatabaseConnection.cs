26730152db621d2ec9f386a067b1dc95c999c803
T00225685 <dannyfinnegan60@gmail.com]>
2024-10-15 17:18:41 +0100

    THIS WILL BREAK THE CODE DO NOT PUSH OVER IT

--- BudgieBudgeting/Pages/DatabaseConnection.cs
+++ BudgieBudgeting/Pages/DatabaseConnection.cs
@@ -4,4 +4,4 @@ namespace BudgieBudgeting
     {
         public static Microsoft.Data.SqlClient.SqlConnection Connection { get; } = new Microsoft.Data.SqlClient.SqlConnection("Server=tcp:budgie-budgeting.database.windows.net,1433;Initial Catalog=Budgie;Persist Security Info=False;User ID=Budgie;Password=Budgeting12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
     }
-}
+
