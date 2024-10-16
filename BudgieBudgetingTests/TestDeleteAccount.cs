f43a3d0689862e409f3a632e9c5664ab24120a4a
Laura Looney <Laura.Looney@students.ittralee.ie>
2024-10-16 13:09:49 +0100

    Added Delete account logic Test

---
+++ BudgieBudgetingTests/TestDeleteAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgieBudgetingTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BudgieBudgeting.Pages.Shared;
+    using BudgieBudgeting.Pages;
+
+    [TestClass]
+    public class TestDeleteAccount
+    {
+        [TestMethod]
+        public void TestDelete_Account()
+        {
+
+            var deleteMod = new Delete_AccountModel
+            {
+                DeleteCredential = new DeleteCredential() 
+                {
+                    Email = "dannyfinnegan60@gmail.com",
+                    Password = "Please work",
+
+                },
+                ErrorMessage = string.Empty
+            };
+
+            deleteMod.OnPost();
+
+            Assert.IsTrue(true);
+        }
+    }
+}
