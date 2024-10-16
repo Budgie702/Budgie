f51d8fd8c6e8d6170b1351a882909ef43fa11569
TurboNatee <nathan.swanepoel@students.ittralee.ie>
2024-10-16 18:04:02 +0100

    Fixed error message name from Username to Email

--- BudgieBudgeting/Pages/login.cshtml.cs
+++ BudgieBudgeting/Pages/login.cshtml.cs
@@ -58,7 +58,7 @@ namespace BudgieBudgeting.Pages.Shared
                             }
                             else
                             {
-                                ErrorMessage = "Invalid username";
+                                ErrorMessage = "Invalid Email";
                             }
                         }
                     }
