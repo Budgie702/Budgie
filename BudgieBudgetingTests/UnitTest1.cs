686ee9338296aab7d17d5ed35c35f4b857caa340
T00225685 <T00225685@1XXCC96E52C1240>
2024-10-16 10:18:03 +0100

    Added Homepage Get Test

--- BudgieBudgetingTests/UnitTest1.cs
+++ BudgieBudgetingTests/UnitTest1.cs
@@ -1,11 +1,19 @@
 namespace BudgieBudgetingTests
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using BudgieBudgeting.Pages.Shared;
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetHomepage()
         {
+            var homepageModel = new HomepageModel();
+
+            homepageModel.OnGet();
+
+            Assert.IsTrue(true);
         }
     }
 }
