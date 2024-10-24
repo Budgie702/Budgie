db5624ef76d951d7b1f0ee1dbe0dc3a112b08814
Danny Finnegan <dannyfinnegan60@gmail.com>
2024-10-24 11:05:55 +0100

    Add .editorconfig, update solution, and refactor DB config
    - Added .editorconfig with C# coding conventions and rules.
    - Updated BudgieBudgeting.sln to include BudgieBudgetingTests and a new solution folder "Solution Items".
    - Modified BudgieBudgeting.csproj to include a new COM reference.
    - Refactored DatabaseConnection.cs to use appsettings.json for connection string.
    - Updated appsettings.json with new ConnectionStrings section.
    - Added web.config with encrypted connection string settings.

--- BudgieBudgeting/Pages/DatabaseConnection.cs
+++ BudgieBudgeting/Pages/DatabaseConnection.cs
@@ -1,7 +1,15 @@
 namespace BudgieBudgeting
 {
+    using System.Configuration;
+    using System.Collections.Specialized;
     public static class DatabaseConnection
     {
-        public static Microsoft.Data.SqlClient.SqlConnection Connection { get; } = new Microsoft.Data.SqlClient.SqlConnection("Server=tcp:budgie-budgeting.database.windows.net,1433;Initial Catalog=Budgie;Persist Security Info=False;User ID=Budgie;Password=Budgeting12345;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        static IConfigurationRoot Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        static string? ConnectionString = Configuration.GetConnectionString("BudgieBudgetingContext");
+        public static Microsoft.Data.SqlClient.SqlConnection Connection { get; } = new Microsoft.Data.SqlClient.SqlConnection(ConnectionString!);
     }
 }
