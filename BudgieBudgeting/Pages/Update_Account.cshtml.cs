ed7a851b4a9f6b1a4b0d05ea978a1d6945315526
TurboNatee <nathan.swanepoel@students.ittralee.ie>
2024-11-05 19:12:13 +0000

    Fixed Update_Account, this now updates the Customer, then ends the session to redirect to the login page.

--- BudgieBudgeting/Pages/Update_Account.cshtml.cs
+++ BudgieBudgeting/Pages/Update_Account.cshtml.cs
@@ -1,17 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
-using System.Data.Common;
 using Microsoft.Data.SqlClient;
-using System.Diagnostics.Eventing.Reader;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
 
 namespace BudgieBudgeting.Pages
 {
     public class Update_AccountModel : PageModel
     {
-
         private readonly DatabaseConnection _databaseConnection;
 
         public Update_AccountModel(DatabaseConnection databaseConnection)
@@ -20,33 +16,54 @@ namespace BudgieBudgeting.Pages
         }
 
         public string? ErrorMessage { get; set; }
-        UpdateCredential updateCredential { get; set; }
+
+        // Make updateCredential public so it's accessible in the Razor Page
+        [BindProperty]
+        public UpdateCredential updateCredential { get; set; } = new UpdateCredential();
 
         public virtual void OnGet()
         {
             // Retrieve the username from the session
-            String Username = HttpContext.Session.GetString("Username"); // Use session to retrieve username
+            string Username = HttpContext.Session.GetString("Username");
 
-            string query = "Select CustomerId,Email,Password form dbo.Customer where @Username = Username";
+            if (string.IsNullOrEmpty(Username))
+            {
+                ErrorMessage = "No user logged in.";
+                return;
+            }
+
+            string query = "SELECT CustomerId, Email, UserPassword FROM dbo.Customer WHERE Username = @Username";
+
             using (SqlConnection connection = new SqlConnection(_databaseConnection.Connection.ConnectionString))
             {
-                using (SqlCommand command = new SqlCommand(query,connection))
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Username", Username);
                     connection.Open();
-                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-                    connection.Close();
-                    DataTable table = new DataTable();
-                    dataAdapter.Fill(table);
-                    updateCredential.Username = Username;
-                    updateCredential.CustomerId = Convert.ToInt32(table.Rows[0][0]);
-                    updateCredential.Email = table.Rows[0][1].ToString();
-                    updateCredential.Password = table.Rows[0][2].ToString();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            updateCredential.Username = Username;
+                            updateCredential.CustomerId = reader.GetInt32(0);
+                            updateCredential.Email = reader.GetString(1);
+                            updateCredential.Password = reader.GetString(2);
+                            Console.WriteLine($"Email: {updateCredential.Email}");
+                            Console.WriteLine($"Username: {updateCredential.Username}");
+                            Console.WriteLine($"Password: {updateCredential.Password}");
+                            Console.WriteLine($"CustomerId: {updateCredential.CustomerId}");
+                        }
+                        else
+                        {
+                            ErrorMessage = "User data not found.";
+                        }
+                    }
                 }
             }
         }
 
-        public IActionResult OnPost(UpdateCredential credential)
+        public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
@@ -55,37 +72,46 @@ namespace BudgieBudgeting.Pages
 
             using (SqlConnection connection = new SqlConnection(_databaseConnection.Connection.ConnectionString))
             {
-                string query = "UPDATE dbo.Customer SET Email = @Email, Username = @Username, UserPassword = @Password WHERE @Id = CustomerId";
+                string query = "UPDATE dbo.Customer SET Email = @Email, Username = @Username, UserPassword = @Password WHERE CustomerId = @Id";
+
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@Email", credential.Email);
-                    command.Parameters.AddWithValue("@Username", credential.Username);
-                    command.Parameters.AddWithValue("@Password", credential.Password);
-                    command.Parameters.AddWithValue("@Id",credential.CustomerId);
+                    command.Parameters.AddWithValue("@Email", updateCredential.Email);
+                    command.Parameters.AddWithValue("@Username", updateCredential.Username);
+                    command.Parameters.AddWithValue("@Password", updateCredential.Password);
+                    command.Parameters.AddWithValue("@Id", updateCredential.CustomerId);
 
                     connection.Open();
                     int rowsAffected = command.ExecuteNonQuery();
-                    connection.Close();
 
                     if (rowsAffected > 0)
                     {
-                        return RedirectToPage("Homepage");
+
+                        HttpContext.Session.Clear();
+
+
+                        return Content(@"<script>
+                                    alert('Update successful! You will now be redirected to the login page.');
+                                    window.location.href = '/Login';
+                                 </script>", "text/html");
                     }
                     else
                     {
-                        ErrorMessage = "Invalid password";
+                        ErrorMessage = "Update failed. Please try again.";
                     }
                 }
             }
 
-            ErrorMessage = "Invalid email";
             return Page();
         }
+
     }
+
     public class UpdateCredential
     {
         [Required]
         public string Username { get; set; }
+
         [Required]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
@@ -93,8 +119,8 @@ namespace BudgieBudgeting.Pages
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
         [Required]
-        public int CustomerId {  get; set; }
+        public int CustomerId { get; set; }
     }
 }
-
